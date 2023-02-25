b55d86fa6ea03f7cf727d949babf0c29928d8489
carlosruiz-stack <122636522+carlosruiz-stack@users.noreply.github.com>
2023-02-25 16:32:44 +0100

    Update sakila.movie.rental-25.02.sql
    add comments to the queries

--- sakila.movie.rental-25.02.sql
+++ sakila.movie.rental-25.02.sql
@@ -1,25 +1,36 @@
 USE sakila; 
 
+#1 Drop column picture from staff
+
 ALTER TABLE staff 
 DROP COLUMN picture;
 
+#2 A new person is hired to help Jon. Her name is TAMMY SANDERS, and she is a customer. Update the database accordingly.
 
 INSERT INTO staff 
 VALUES (1,'tammy','sanders',2,'tammy.sanders@blockbuster.us',3,1,'tammysanders','123456',timestamp);
 
-#16:22:14	INSERT INTO staff  VALUES (1,'tammy','sanders',2,'tammy.sanders@blockbuster.us',3,1,'tammysanders','123456',timestamp)	Error Code: 1054. Unknown column 'timestamp' in 'field list'	0,000 sec
+INSERT INTO staff  VALUES (1,'tammy','sanders',2,'tammy.sanders@blockbuster.us',3,1,'tammysanders','123456',timestamp)	Error Code: 1054. Unknown column 'timestamp' in 'field list'	0,000 sec
 
 
 #3 stuck 
 
-#4 b) and c) unable to do 
+#4 
+
+a) Check if where are non-active users
 
 SELECT customer_id FROM customer
 WHERE active = 0;
+
+b) Create backup table, deleted_users
 
 CREATE TABLE deleted_users (
 customer_id smallint,
 email varchar(50),
 date datetime
 );
+
+c) Insert the non active users in the table backup table
+
+d) Delete the non active users from the table customer
 
